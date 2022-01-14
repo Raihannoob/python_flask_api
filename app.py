9d002ed50762072004c71cf9f303fcf81230a572
Raihannoob <raihan.tanvir961@gmail.com>
2022-01-14 16:28:13 +0600

    updated Some bug

--- app.py
+++ app.py
@@ -24,44 +24,8 @@ SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
 )
 app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
 
-# Create some test data for our catalog in the form of a list of dictionaries.
-# books = [
-#     {'id': 0,
-#      'title': 'A Fire Upon the Deep',
-#      'author': 'Vernor Vinge',
-#      'first_sentence': 'The coldsleep itself was dreamless.',
-#      'year_published': '1992'},
-#     {'id': 1,
-#      'title': 'The Ones Who Walk Away From Omelas',
-#      'author': 'Ursula K. Le Guin',
-#      'first_sentence': 'With a clamor of bells that set the swallows soaring, the Festival of Summer came to the city Omelas, bright-towered by the sea.',
-#      'published': '1973'},
-#     {'id': 2,
-#      'title': 'Dhalgren',
-#      'author': 'Samuel R. Delany',
-#      'first_sentence': 'to wound the autumnal city.',
-#      'published': '1975'}
-# ]
 
-# @app.route('/api/v1/resources/books/all', methods=['GET'])
-# def api_all():
-#     return jsonify(books)
-# @app.route('/api/v1/resources/books', methods=['GET'])
-# def api_id():
-#     if 'id' in request.args:
-#         id = int(request.args['id'])
-#     else:
-#         return "Error: No id field provided. Please specify an id."
-#     results = []
-#     for book in books:
-#         if book['id'] == id:
-#             results.append(book)
-#     return jsonify(results) 
-@app.route('/protected')
-def protected():
-    #return jsonify({'message': 'Only available to people with valid tokens.'})
 
-    return redirect("http://127.0.0.1:5000/swagger",code=302)
 
 
 
@@ -97,6 +61,14 @@ def login():
 def dashboard():
     # we will Show Swagger UI here
     return "login successfully"
+
+
+@app.route('/protected')
+def protected():
+    #return jsonify({'message': 'Only available to people with valid tokens.'})
+
+    return redirect("http://127.0.0.1:5000/swagger",code=302)
+
 
 #for Databse 
 @app.route('/data', methods=['GET'])
