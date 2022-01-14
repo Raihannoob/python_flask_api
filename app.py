9b9ea2093ac29f730ae4018e01a070add1cd9283
Raihannoob <raihan.tanvir961@gmail.com>
2022-01-14 11:25:59 +0600

    databse  running successfully

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 import flask
 from flask import Flask, jsonify, request, make_response, redirect,render_template
-import mysql.connector
-from mysql.connector import cursor
 import jwt
 import datetime
 from functools import wraps
-app = flask.Flask(__name__)
+from mysql.connector import cursor
+import mysql.connector
+
+import json
+app = Flask(__name__)
 app.config['DEBUG'] = True
 
 # @app.route('/form')
@@ -97,6 +99,103 @@ app.config['DEBUG'] = True
 #         if book['id'] == id:
 #             results.append(book)
 #     return jsonify(results) 
+# @app.route('/data', methods=['GET'])
+# def house():
+#    # title bedroom sleeps bathroom price location
+#     hoteldata=[]
+
+#     db_connector = mysql.connector.connect(
+#         host="localhost",
+#         user="root",
+#         password="",
+#         database="Scrapping"
+#     )
+
+
+#     title = request.args.get('title')
+#     bedroom = request.args.get('bedroom')
+#     sleep = request.args.get('sleeps')
+#     bathroom = request.args.get('bathroom')
+#     price = request.args.get('price')
+#     location = request.args.get('location')
+
+#     cursor = db_connector.cursor()
+# # sleep  	bedroom  	bathroom  	price title
+#     sql= "SELECT * FROM datatable WHERE "
+#     valudata=0
+
+#     if(title!=None):
+#         if(sql=='SELECT * FROM datatable WHERE '):
+#             sql=sql+"title="+f"'{title}'"
+#         else:
+#             sql = sql + "AND title=" + f"' {title}'"
+
+#     if(location!=None):
+#         if (sql == 'SELECT * FROM datatable WHERE '):
+#             sql=sql+"location="+f"'{location}'"
+#         else:
+#             sql = sql + "AND location=" + f"'{location}'"
+#     if(bedroom!=None):
+#         if (sql == 'SELECT * FROM datatable WHERE '):
+#             sql=sql+"bedroom="+f"'{bedroom}'"
+#         else:
+#             sql = sql + "AND bedroom>=" + f"'{bedroom}'"
+#     if(bathroom!=None):
+#         if (sql == 'SELECT * FROM datatable WHERE '):
+#             sql=sql+"bathroom>="+f"'{bathroom}'"
+#         else:
+#             sql = sql + "AND bathroom>=" + f"'{bathroom}'"
+#     if(sleep!=None):
+#         if (sql == 'SELECT * FROM datatable WHERE '):
+#             sql=sql+"sleep>="+f"'{sleep}'"
+#         else:
+#             sql = sql + "AND sleep>=" + f"'{sleep}'"
+#     if(price!=None):
+#         if (sql == 'SELECT * FROM datatable WHERE '):
+#             sql=sql+"price>="+f"'${price}'"
+#         else:
+#             sql = sql + "AND price>=" + f"'${price}'"
+
+
+#     # query = ("SELECT * FROM datatable WHERE location ="+f"'{location}'  OR price=" + f"'{price}' OR bedroom=" + f"'{bedroom}'OR bathroom=" + f"'{bathroom}' OR sleep=" + f"'{sleeps}'")
+#     query=sql
+#     print(query)
+#     cursor.execute(query)
+
+#     results = cursor.fetchall()
+
+#     for x in results:
+#         data = {
+#             "Title": x[0],
+#             "Location": x[1],
+#             "Sleeps": x[2],
+#             "Bedrooms": x[3],
+#             "Bathrooms": x[4],
+#             "Price": x[5],
+#             "Picture": {
+#                 "Picture_1": x[6],
+#                 "Picture_2": x[7],
+#                 "Picture_3": x[8],
+#             }
+
+#         }
+#         hoteldata.append(data)
+
+#     # Json = json.dumps(hoteldata, indent=4)
+
+
+#     hoteldata.sort(key=lambda x: x["Price"])
+
+
+#     Json = json.dumps(hoteldata, indent=4)
+#     print(Json)
+#     # print(title, bedroom, sleeps, location)
+#     return Json
+
+
+
+
+
 
 @app.route('/login')
 def form():
@@ -123,6 +222,83 @@ def login():
 def dashboard():
     # we will Show Swagger UI here
     return "login successfully"
+
+#for Databse 
+@app.route('/data', methods=['GET'])
+def house():
+   # name location ratting prices Amenities images
+    hoteldata=[]
+
+    db_connector = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="",
+        database="Scrap"
+    )
+
+    name = request.args.get('name')
+    location = request.args.get('location')
+    ratting = request.args.get('ratting')
+    prices = request.args.get('prices')
+    Amenities = request.args.get('Amenities')
+    images = request.args.get('images')
+
+    cursor = db_connector.cursor()
+
+    sql= "SELECT * FROM data_from_web WHERE "
+    valudata=0
+
+    if(name!=None):
+        if(sql=='SELECT * FROM data_from_web WHERE '):
+            sql=sql+"name="+f"'{name}'"
+        else:
+            sql = sql + "AND title=" + f"' {name}'"
+
+    if(location!=None):
+        if (sql == 'SELECT * FROM data_from_web WHERE '):
+            sql=sql+"location="+f"'{location}'"
+        else:
+            sql = sql + "AND location=" + f"'{location}'"
+    if(ratting!=None):
+        if (sql == 'SELECT * FROM data_from_web WHERE '):
+            sql=sql+"ratting="+f"'{ratting}'"
+        else:
+            sql = sql + "AND ratting=" + f"'{ratting}'"
+    if(prices!=None):
+        if (sql == 'SELECT * FROM data_from_web WHERE '):
+            sql=sql+"prices>="+f"'{prices}'"
+        else:
+            sql = sql + "AND prices=" + f"'{prices}'"
+    if(Amenities!=None):
+        if (sql == 'SELECT * FROM data_from_web WHERE '):
+            sql=sql+"Amenities>="+f"'{Amenities}'"
+        else:
+            sql = sql + "AND Amenities=" + f"'{Amenities}'"
+    if(images!=None):
+        if (sql == 'SELECT * FROM data_from_web WHERE '):
+            sql=sql+"images="+f"'${images}'"
+        else:
+            sql = sql + "AND images=" + f"'${images}'"
+
+    query=sql
+    print(query)
+    cursor.execute(query)
+
+    results = cursor.fetchall()
+
+    for x in results:
+         # name location ratting prices Amenities images
+        data = {
+            "name": x[0],
+            "location": x[1],
+            "ratting": x[2],
+            "prices": x[3],
+            "Amenities":x[4],
+            "images": x[5]
+            }
+        hoteldata.append(data)
+    json = jsonify(hoteldata)
+    return json
 
        
 app.run()
